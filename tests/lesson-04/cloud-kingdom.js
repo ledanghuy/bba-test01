46a4dad0a3efd7379f65875233280bcbac6e1f99
test 01-Huy <ledanghuy91@gmail.com>
2026-01-28 22:56:13 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,18 @@
+console.log("Bài 2:");
+
+let powerUp = "star";
+let effect = "";
+
+if (powerUp === "mushroom") {
+    effect = " Mario becomes Super!";
+} else if (powerUp === "flower") {
+    effect = "Mario can shoot fireballs!";
+} else if (powerUp === "star") {
+    effect = "Mario is invincible!";
+} else if (powerUp === "none") {
+    effect = "Mario is normal";
+} else {
+    effect = "Unknown power-up";
+}
+
+console.log(effect);
