94b370f8430117fd55e70b8fc7274ee34d187785
test 01-Huy <ledanghuy91@gmail.com>
2026-01-31 13:52:05 +0700

    update bai 3.1

--- tests/lesson-04/cloud-kingdom.js
+++ tests/lesson-04/cloud-kingdom.js
@@ -2,13 +2,13 @@ console.log("Bài 1:");
 
 let playName = "Mario";
 let currentLives = 3;
-const level1Coin = 25;
-const level2Coin = 30;
-const level3Coin = 45;
+const level_1_Coin = 25;
+const level_2_Coin = 30;
+const level_3_Coin = 45;
 
 console.log("Tính tổng coin của 3 leve sau đó tính giá trị trung bình");
 
-let sumCoin = level1Coin + level2Coin + level3Coin;
+let sumCoin = level_1_Coin + level_2_Coin + level_3_Coin;
 let tbCoin = sumCoin / 3;
 
 console.log(`Tổng coin của 3 level là : ${sumCoin} `);
